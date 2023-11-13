4b0566a1dd2a61814932a7f8e91a7ba60b4cc9b4
Andre Weissflog <floooh@gmail.com>
2023-11-13 17:09:38 +0100

    running asmx works

---
+++ src/asmx.ts
@@ -0,0 +1,38 @@
+import { workspace, ExtensionContext, Uri, window } from 'vscode';
+import { Wasm } from '@vscode/wasm-wasi';
+
+async function findMainSources(): Promise<string | undefined> {
+    const uris = await workspace.findFiles('src/main.asm', null, 1);
+    return (uris.length === 1) ? uris[0].fsPath : undefined;
+}
+
+export async function run(context: ExtensionContext, wasm: Wasm, args: string[]) {
+    const pty = wasm.createPseudoterminal();
+    const terminal = window.createTerminal({ name: 'asmx', pty, isTransient: true });
+    terminal.show(true);
+    try {
+        const bits = await workspace.fs.readFile(Uri.joinPath(context.extensionUri, 'media/asmx.wasm'));
+        const module = await WebAssembly.compile(bits);
+        const fs = await wasm.createRootFileSystem([ { kind: 'workspaceFolder' }]);
+        const process = await wasm.createProcess('asmx', module, {
+            rootFileSystem: fs,
+            stdio: pty.stdio,
+            args,
+        });
+        const result = await process.run();
+    } catch (err) {
+        window.showErrorMessage((err as Error).message);
+    }
+}
+
+export async function assemble(context: ExtensionContext, wasm: Wasm) {
+    console.log('Assemble called!');
+    const src = await findMainSources();
+    if (src === undefined) {
+        window.showErrorMessage('No project main file found (must be "src/main.asm")');
+        return;
+    }
+    console.log(`assembling: ${src}`);
+    await run(context, wasm, [ '-l', '-o', '-w', '-e', '-C', 'z80', '/workspace/src/main.asm' ]);
+    console.log('done.');
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,16 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { Wasm } from '@vscode/wasm-wasi';
+import { assemble } from './asmx';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-asmx" is now active!');
+	console.log('vscode-asmx: activate() called');
+	const wasm = await Wasm.load();
+	console.log('vscode-asmx: Wasm object loaded');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-asmx.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-asmx!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('vscode-asmx.assemble', async () => {
+		await assemble(context, wasm);
+	}));
 }
 
 // This method is called when your extension is deactivated
